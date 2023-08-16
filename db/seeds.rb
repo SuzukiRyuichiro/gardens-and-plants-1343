0a1b1b23401688298dcd546c6ace4b2c0cd9796e
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-16 19:42:09 +0100

    generate tag model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,32 @@
 # db/seeds.rb
 Garden.destroy_all if Rails.env.development?
 
-garden1 = Garden.create!(
+Garden.destroy_all if Rails.env.development?
+
+little = Garden.create!(
   name: 'My Little Garden',
   banner_url: 'https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg'
 )
 
-Garden.create!(
+other = Garden.create!(
   name: 'My Other Garden',
   banner_url: 'https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg'
 )
 
 Plant.create!(
-  name: 'Cactus',
-  image_url: 'https://picsum.photos/200',
-  garden: garden1
+  name: 'Monstera',
+  image_url: 'https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg',
+  garden: little
 )
 
 Plant.create!(
-  name: 'Oak',
-  image_url: 'https://picsum.photos/200',
-  garden: garden1
+  name: 'Philo',
+  image_url: 'https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/philo.jpg',
+  garden: little
 )
 
 Plant.create!(
-  name: 'Palm Tree',
-  image_url: 'https://picsum.photos/200',
-  garden: garden1
+  name: 'Dieff',
+  image_url: 'https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/dieffenbachia.jpg',
+  garden: other
 )
